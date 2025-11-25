54afc3fa13142169ff60111b8d2db5a1d4ee1129
Jules Claudon <julesclaudon@gmail.com>
2025-11-25 17:24:43 +0100

    Add ChatsController + Views

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,53 @@
 class ChatsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_chat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @chats = Chat.all.order(created_at: :desc)
+  end
+
+  def show
+    @messages = @chat.messages.order(created_at: :asc)
+    @musics   = @chat.musics
+  end
+
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = current_user.chats.new(chat_params)
+    if @chat.save
+      redirect_to @chat, notice: "Session musicale créée avec succès !"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @chat.update(chat_params)
+      redirect_to @chat, notice: "Session mise à jour !"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @chat = Chat.find(params[:id])
+    @chat.destroy
+    redirect_to chats_path, notice: "Session supprimée."
+  end
+
+  private
+
+  def set_chat
+    @chat = Chat.find(params[:id])
+  end
+
+  def chat_params
+    params.require(:chat).permit(:title)
+  end
 end
