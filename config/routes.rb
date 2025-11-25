25f38506884e3dc4f40476dc51f0a8218eff5e24
Hugo Prioux <hugss_prioux@live.fr>
2025-11-25 16:16:44 +0100

    profile controller / view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+# Route pour le profil (utiliser la ressource 'resource' car il n'y a qu'un profil par utilisateur connecté)
+  resource :profile, only: [:show] do
+    # Actions spécifiques non-RESTful
+    get :favorites, on: :collection
+    get :settings, on: :collection
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
