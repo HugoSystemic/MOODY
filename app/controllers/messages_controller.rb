b04c142b1657adfa7855a7e0cac478d59a652ce9
Jasky Sor <jaskysor@gmail.com>
2025-11-26 16:14:03 +0100

    fix prompt

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   # before_action :set_chat
 
   SYSTEM_PROMPT_FOR_ACTIVITIES = "Can you find an activity and a duration depending on that message \n\n Please return a json format with this keys : { 'message'=> une réponse récapitulant les infos du message du user, 'activity'=> an activity, 'duration'=> a duration in seconds, 'found'=> true/false if you managed to find an activity and a duration }\n\n the key 'message' should return a message with a link to a youtube video that match my mood, activity and duration\n\n check that the video is still accessible"
-  SYSTEM_PROMPT_MUSIC_URLS = "Return a message with a link to a youtube video that match my mood, activity and duration"
+  SYSTEM_PROMPT_MUSIC_URLS = "Return a message with a link to a youtube video that match my mood, activity and duration\n\n check that the video is still accessible on youtube"
 
   # GET /chats/:chat_id/messages
   def index
@@ -17,13 +17,13 @@ class MessagesController < ApplicationController
     @chat = Chat.find(params[:chat_id])
     @message = Message.new(message_params)
     @message.chat = @chat
-    @message.role = 'user' # Le rôle de l'utilisateur est fixé ici
+    @message.role = 'user'
 
     if @message.save
       @ruby_llm_chat = RubyLLM.chat
       build_conversation_history()
 
-      if @chat.messages.where(role: 'assistant').size == 1 || @chat.messages.where(role: 'assistant').size.zero?
+      if @chat.messages.where(role: 'assistant').size == 1
         response = @ruby_llm_chat.with_instructions(SYSTEM_PROMPT_FOR_ACTIVITIES).ask(@message.content)
         parsed_response = JSON.parse(response.content)
         if parsed_response["found"] == true
@@ -40,30 +40,6 @@ class MessagesController < ApplicationController
     else
       render "chats/show", status: :unprocessable_entity
     end
-
-
-    # if @chat.messages.where(role: 'assistant').size == 1
-    #   # mettre à jour le mood du chat
-    # elsif @chat.messages.where(role: 'assistant').size == 2
-    #   # mettre à jour activity et duration
-    # end
-
-    # if @message.save
-    #     unless @chat.parameters_complete?
-    #       extract_session_parameters
-    #     end
-    #     if @chat.parameters_complete?
-    #       process_music_generation
-    #       redirect_to chat_path(@chat)
-    #     else
-    #       ask_for_missing_parameters
-    #       redirect_to chat_messages_path(@chat)
-    #     end
-    #   else
-    #     @messages = @chat.messages.order(created_at: :asc)
-    #     render :index, status: :unprocessable_entity
-    #   end
-    # end
   end
 
   private
