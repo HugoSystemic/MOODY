8d4792f5729fe95ea4a43f3afa17ea30c3f80e02
Hugo Prioux <hugss_prioux@live.fr>
2025-11-27 15:47:48 +0100

    favoris configurés

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resource :profile, only: [:show, :update] do
-    get :settings
-    get :favorites
+  resource :profile, only: [:show] do
+    get :favorites, on: :collection
+    get :settings, on: :collection
   end
 
   resources :chats, only: [:index, :new, :create, :show, :destroy] do
     resources :messages, only: [:index, :create]
+    member do
+      # Ajout de la route pour basculer l'état "liked"
+      patch :toggle_favorite
+    end
   end
 
   resources :musics, only: [:index]
 
-  resources :chats
+  resources :favorites, only: [:index], controller: 'favorites'
 end
